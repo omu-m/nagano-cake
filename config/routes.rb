93d9faa98a4dee569b0423f6379376cf7af86bb8
MoekaOmura <110664281+omu-m@users.noreply.github.com>
2022-09-24 00:59:00 +0000

    views/layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,61 +1,41 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'orders/show'
-  end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-  end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/edit'
-  end
-  namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/show'
-    get 'items/edit'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :public do
-    get 'addresses/index'
-    get 'addresses/edit'
-  end
-  namespace :public do
-    get 'orders/new'
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :public do
-    get 'cart_items/index'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-  end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
+
   # devise_for :admins
   # devise_for :customers
+
+  # 管理者側
+  namespace :admin do
+    get "/" => "homes#top"
+    resources :orders
+    resources :customers
+    resources :genres
+    resources :items
+  end
+
+  # 会員側
+  scope module: :public do
+    root to: "homes#top"
+    get "/about" => "homes#about", as: "about"
+    resources :addresses
+    resources :orders
+    resources :cart_items
+    resources :items
+    get "/customers/mypage" => "customers#show"
+    get "/customers/mypage/edit" => "customers#edit"
+    get "/customers/unsubscribe" => "customers#unsubscribe"
+    get "/customers/withdrawal" => "customers#withdrawal"
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  # 顧客用
+  # 会員側
   # URL /customers/sign_in ...
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
 
-  # 管理者用
+  # 管理者側
   # URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
